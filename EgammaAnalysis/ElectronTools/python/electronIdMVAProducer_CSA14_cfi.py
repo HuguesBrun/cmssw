0ef30be600e5b7533c883d67cb39f76bc5f65153
Hugues <hbrun@cern.ch>
2014-08-27 10:11:01 +0200

    update to have scenario 1 and 2

--- EgammaAnalysis/ElectronTools/python/electronIdMVAProducer_CSA14_cfi.py
+++ EgammaAnalysis/ElectronTools/python/electronIdMVAProducer_CSA14_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-mvaTrigV0CSA14 = cms.EDFilter("ElectronIdMVAProducerCSA14",
+mvaTrigV0CSA14_50ns = cms.EDFilter("ElectronIdMVAProducerCSA14",
                          verbose = cms.untracked.bool(False),
                          vertexTag = cms.InputTag('offlinePrimaryVertices'),
                          electronTag = cms.InputTag('gedGsfElectrons'),
@@ -8,13 +8,25 @@ mvaTrigV0CSA14 = cms.EDFilter("ElectronIdMVAProducerCSA14",
                          reducedEERecHitCollection = cms.InputTag('reducedEcalRecHitsEE'),
                          method = cms.string("BDT"),
                          mvaWeightFile = cms.vstring(
-                                                     "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_EB_BDT.weights.xml",
-                                                     "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_EE_BDT.weights.xml",
+                                                     "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_50ns_EB_BDT.weights.xml",
+                                                     "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_50ns_EE_BDT.weights.xml",
     ),
                          Trig = cms.bool(True),
 )
 
-
+mvaTrigV0CSA14_25ns = cms.EDFilter("ElectronIdMVAProducerCSA14",
+                                   verbose = cms.untracked.bool(False),
+                                   vertexTag = cms.InputTag('offlinePrimaryVertices'),
+                                   electronTag = cms.InputTag('gedGsfElectrons'),
+                                   reducedEBRecHitCollection = cms.InputTag('reducedEcalRecHitsEB'),
+                                   reducedEERecHitCollection = cms.InputTag('reducedEcalRecHitsEE'),
+                                   method = cms.string("BDT"),
+                                   mvaWeightFile = cms.vstring(
+                                                               "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_25ns_EB_BDT.weights.xml",
+                                                               "EgammaAnalysis/ElectronTools/data/CSA14/TrigIDMVA_25ns_EE_BDT.weights.xml",
+                                                               ),
+                                   Trig = cms.bool(True),
+                                   )
 
 #mvaNonTrigV0CSA14 = cms.EDFilter("ElectronIdMVAProducerCSA14",
 #                            verbose = cms.untracked.bool(False),
