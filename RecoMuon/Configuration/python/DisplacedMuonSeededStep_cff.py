f20e0b3ec5a12d0f816637dc9677d727f12e1980
Hugues <hbrun@cern.ch>
2017-03-30 14:24:30 +0200

    adding back the merging of the displace OutIn with the InOut iteration in phase2

--- RecoMuon/Configuration/python/DisplacedMuonSeededStep_cff.py
+++ RecoMuon/Configuration/python/DisplacedMuonSeededStep_cff.py
@@ -16,7 +16,7 @@ import TrackingTools.KalmanUpdators.Chi2MeasurementEstimator_cfi
 muonSeededMeasurementEstimatorForOutInDisplaced = TrackingTools.KalmanUpdators.Chi2MeasurementEstimator_cfi.Chi2MeasurementEstimator.clone(
     ComponentName = cms.string('muonSeededMeasurementEstimatorForOutInDisplaced'),
     MaxChi2 = cms.double(30.0), ## was 30 ## TO BE TUNED
-    nSigma  = cms.double(3.),    ## was 3  ## TO BE TUNED 
+    nSigma  = cms.double(3.),    ## was 3  ## TO BE TUNED
 )
 
 ###------------- TrajectoryFilter, defining selections on the trajectories while building them ----------------
@@ -28,15 +28,15 @@ muonSeededTrajectoryFilterForOutInDisplaced.minimumNumberOfHits = 5 ## allow mor
 ###------------- TrajectoryBuilders ----------------
 #for displaced global muons
 muonSeededTrajectoryBuilderForOutInDisplaced = RecoTracker.CkfPattern.GroupedCkfTrajectoryBuilder_cfi.GroupedCkfTrajectoryBuilder.clone(
-    foundHitBonus = cms.double(1000.0),  
-    lostHitPenalty = cms.double(1.0),   
+    foundHitBonus = cms.double(1000.0),
+    lostHitPenalty = cms.double(1.0),
     maxCand   = cms.int32(3),
     estimator = cms.string('muonSeededMeasurementEstimatorForOutInDisplaced'),
     trajectoryFilter = cms.PSet(refToPSet_ = cms.string('muonSeededTrajectoryFilterForOutInDisplaced')),
     inOutTrajectoryFilter = cms.PSet(refToPSet_ = cms.string('muonSeededTrajectoryFilterForOutInDisplaced')), # not sure if it is used
     minNrOfHitsForRebuild    = cms.int32(5),
-    requireSeedHitsInRebuild = cms.bool(True), 
-    keepOriginalIfRebuildFails = cms.bool(False), 
+    requireSeedHitsInRebuild = cms.bool(True),
+    keepOriginalIfRebuildFails = cms.bool(False),
 )
 ######## TRACK CANDIDATE MAKERS
 #for displaced global muons
@@ -48,7 +48,7 @@ muonSeededTrackCandidatesOutInDisplaced = RecoTracker.CkfPattern.CkfTrackCandida
     onlyPixelHitsForSeedCleaner = cms.bool(False),
 )
 
-######## TRACK PRODUCERS 
+######## TRACK PRODUCERS
 #for displaced global muon
 muonSeededTracksOutInDisplaced = RecoTracker.TrackProducer.TrackProducer_cfi.TrackProducer.clone(
     src = cms.InputTag("muonSeededTrackCandidatesOutInDisplaced"),
@@ -60,25 +60,62 @@ muonSeededTracksOutInDisplaced = RecoTracker.TrackProducer.TrackProducer_cfi.Tra
 muonSeededTracksOutInDisplacedClassifier = RecoTracker.IterativeTracking.MuonSeededStep_cff.muonSeededTracksOutInClassifier.clone()
 muonSeededTracksOutInDisplacedClassifier.src='muonSeededTracksOutInDisplaced'
 
+muonSeededTracksOutInDisplacedSelector = RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.multiTrackSelector.clone(
+    src='muonSeededTracksOutInDisplaced',
+    trackSelectors= cms.VPSet(
+        RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.looseMTS.clone(
+            name = 'muonSeededTracksOutInDisplacedLoose',
+            applyAdaptedPVCuts = cms.bool(False),
+            chi2n_par = 10.0,
+            minNumberLayers = 3,
+            min_nhits = 5,
+            maxNumberLostLayers = 4,
+            minNumber3DLayers = 0,
+            minHitsToBypassChecks = 7
+            ),
+        RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.tightMTS.clone(
+            name = 'muonSeededTracksOutInDisplacedTight',
+            preFilterName = 'muonSeededTracksOutInDisplacedLoose',
+            applyAdaptedPVCuts = cms.bool(False),
+            chi2n_par = 1.0,
+            minNumberLayers = 5,
+            min_nhits = 6,
+            maxNumberLostLayers = 3,
+            minNumber3DLayers = 2,
+            minHitsToBypassChecks = 10
+            ),
+        RecoTracker.FinalTrackSelectors.multiTrackSelector_cfi.highpurityMTS.clone(
+            name = 'muonSeededTracksOutInDisplacedHighPurity',
+            preFilterName = 'muonSeededTracksOutInDisplacedTight',
+            applyAdaptedPVCuts = cms.bool(False),
+            chi2n_par = 0.4,
+            minNumberLayers = 5,
+            min_nhits = 7,
+            maxNumberLostLayers = 2,
+            minNumber3DLayers = 2,
+            minHitsToBypassChecks = 20
+            ),
+        ) #end of vpset
+    ) #end of clone
 
 #for displaced global muons
 muonSeededStepCoreDisplaced = cms.Sequence(
     cms.SequencePlaceholder("muonSeededStepCoreInOut") +
-    muonSeededSeedsOutInDisplaced + muonSeededTrackCandidatesOutInDisplaced + muonSeededTracksOutInDisplaced 
+    muonSeededSeedsOutInDisplaced + muonSeededTrackCandidatesOutInDisplaced + muonSeededTracksOutInDisplaced
 )
 
 #for displaced global muons
 muonSeededStepExtraDisplaced = cms.Sequence(
     cms.SequencePlaceholder("muonSeededStepExtraInOut") +
-    muonSeededTracksOutInDisplacedClassifier
+    muonSeededTracksOutInDisplacedSelector
 )
 #for displaced global muons
 muonSeededStepDisplaced = cms.Sequence(
     earlyDisplacedMuons +
     muonSeededStepCoreDisplaced +
-    muonSeededStepExtraDisplaced 
+    muonSeededStepExtraDisplaced
 )
-    
+
 ##### MODULES FOR DEBUGGING ###############3
 #for displaced global muons
 muonSeededSeedsOutInDisplacedAsTracks = cms.EDProducer("FakeTrackProducerFromSeed", src = cms.InputTag("muonSeededSeedsOutInDisplaced"))
--- RecoMuon/Configuration/python/preDuplicateMergingDisplacedTracks_cfi.py
+++ RecoMuon/Configuration/python/preDuplicateMergingDisplacedTracks_cfi.py
@@ -1,6 +1,5 @@
 import FWCore.ParameterSet.Config as cms
 from RecoTracker.FinalTrackSelectors.TrackCollectionMerger_cfi import *
-
 preDuplicateMergingDisplacedTracks = TrackCollectionMerger.clone()
 preDuplicateMergingDisplacedTracks.trackProducers = [
     "muonSeededTracksInOut",
@@ -27,8 +26,31 @@ trackingPhase1PU70.toModify(preDuplicateMergingDisplacedTracks,
 
 # Same for Phase2PU140
 from Configuration.Eras.Modifier_trackingPhase2PU140_cff import trackingPhase2PU140
-trackingPhase2PU140.toModify(preDuplicateMergingDisplacedTracks,
-    trackProducers = [x for x in preDuplicateMergingDisplacedTracks.trackProducers if x != "muonSeededTracksInOut"],
-    inputClassifiers = [x for x in preDuplicateMergingDisplacedTracks.inputClassifiers if x != "muonSeededTracksInOutClassifier"],
-)
+from RecoTracker.FinalTrackSelectors.trackListMerger_cfi import trackListMerger as _trackListMerger
 
+#trackingPhase2PU140.toModify(preDuplicateMergingDisplacedTracks,
+#    trackProducers = [x for x in preDuplicateMergingDisplacedTracks.trackProducers],# if x != "muonSeededTracksInOut"],
+#    inputClassifiers = [x for x in preDuplicateMergingDisplacedTracks.inputClassifiers],# if x != "muonSeededTracksInOutClassifier"],
+#)
+trackingPhase2PU140.toReplaceWith(preDuplicateMergingDisplacedTracks, _trackListMerger.clone(
+    TrackProducers = cms.VInputTag(
+        cms.InputTag("muonSeededTracksInOut"),
+        cms.InputTag("muonSeededTracksOutInDisplaced"),
+    ),
+    hasSelector = cms.vint32(1,1),
+    selectedTrackQuals = cms.VInputTag(
+        cms.InputTag("muonSeededTracksInOutSelector","muonSeededTracksInOutHighPurity"),
+        cms.InputTag("muonSeededTracksOutInDisplacedSelector","muonSeededTracksOutInDisplacedLoose"),
+    ),
+    mvaValueTags = cms.VInputTag(
+        cms.InputTag("muonSeededTracksInOutSelector","MVAVals"),
+        cms.InputTag("muonSeededTracksOutInDisplacedSelector","MVAVals"),
+    ),
+    setsToMerge = cms.VPSet(cms.PSet(pQual = cms.bool(False), tLists = cms.vint32(0, 1))),
+    FoundHitBonus  = 100.0,
+    LostHitPenalty =   1.0,
+    indivShareFrac = cms.vdouble(1.0, 0.16, 0.095, 0.09, 0.095,0.095, 0.095, 0.08),
+    copyExtras = True,
+    makeReKeyedSeeds = cms.untracked.bool(False)
+    )
+)
